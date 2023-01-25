a88ce9424fc66a8529db49bced284a8f9807b735
Michael Ibsen <mrmibsen@gmail.com>
2023-01-25 13:53:00 +0100

    fotos toegevoegd

--- classHotelkamer.py
+++ classHotelkamer.py
@@ -8,8 +8,8 @@ class Hotelkamer:
     def __str__(self):
         return f"{self.kamertype}({self.kamernummer},{self.prijs},{self.beschrijving},{self.foto})"
 
-k1 = Hotelkamer("1 persoonskamer", "101", 150.00, "Een mooie kamer", "foto.jpg")
-k2 = Hotelkamer("2 persoonskamer", "106", 250.00, "Nog een mooie kamer", "foto2.jpg")
+k1 = Hotelkamer("1 persoonskamer", "101", 150.00, "Een mooie kamer", "https://www.dehotelpartners.nl/wp-content/uploads/2019/01/190117_Toekomst_Hotelkamer_1.jpg")
+k2 = Hotelkamer("2 persoonskamer", "106", 250.00, "Nog een mooie kamer", "https://www.dehotelpartners.nl/wp-content/uploads/2019/01/190117_Toekomst_Hotelkamer_2.jpg")
 
 print(k1)
 print(k2)
