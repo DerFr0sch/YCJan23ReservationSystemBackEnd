411af973f0911fd730804039c76df61b3f244b09
David Redder <david-redder@hotmail.com>
2023-02-06 14:21:55 +0000

    Reserveerknop test

---
+++ dbreserveerkamer.py
@@ -0,0 +1,21 @@
+import mysql.connector
+
+def sendKamerreservering(kamerid):
+    con = mysql.connector.connect(
+        host="ycjanhoteldatabase.mysql.database.azure.com",  #port erbij indien mac
+        user="Kevindatahotel",
+        password="abcd1234ABCD!@#$",
+        database="hotel_database"
+    )
+
+    mycursor = con.cursor()
+
+    sql = "UPDATE hotelkamer SET reservering = 1 WHERE kamer_id = "+kamerid+""
+    val = (kamerid)
+    mycursor.execute(sql, val)
+    #print(val,flush=True)
+    con.commit()
+    print(mycursor.rowcount, "Kamer gereserveerd")
+    return "opgeslagen"
+
+#sendKamerinfo("test1")
