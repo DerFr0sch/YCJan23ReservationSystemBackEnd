af2234580cf54415ca0b9dfaad354db2ed66fc49
Ruben <soeters_ruben@hotmail.com>
2023-02-14 14:58:31 +0100

    rubens changes

--- dbboekkamer.py
+++ dbboekkamer.py
@@ -22,5 +22,5 @@ def sendKamerboeking(kamerid, totprijs, boeking_begin, boeking_eind, memberid, b
     mycursor.execute(sql)
     
     myresult = mycursor.fetchall()
-    
-    return str(str(myresult[0][0])+"/"+str(kamerid)+"/"+str(totprijs)+"/"+boeking_begin+"/"+boeking_eind+"/"+str(memberid)+"/"+betaalmet) 
+    print(str(str(myresult[0][0])+"/"+str(kamerid)+"/"+str(totprijs)+"/"+boeking_begin+"/"+boeking_eind+"/"+str(memberid)+"/"+betaalmet) )
+    return [str(myresult[0][0]), str(kamerid), str(totprijs), boeking_begin, boeking_eind, str(memberid), betaalmet]
