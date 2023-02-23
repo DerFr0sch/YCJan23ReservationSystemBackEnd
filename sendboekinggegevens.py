ae6b6799e0fe6c726570a84a261d408d5e223227
ruben <soeters_ruben@hotmail.com>
2023-02-23 14:43:13 +0100

    weizigingen in boekingspage

--- sendboekinggegevens.py
+++ sendboekinggegevens.py
@@ -10,7 +10,9 @@ def getBoekinggegevens(memberid):
     )
 
     mycursor = con.cursor()
-    mycursor.execute("SELECT * FROM boeking WHERE member_id="+memberid)
+    mycursor.execute("SELECT * FROM boeking INNER JOIN hotelkamer ON boeking.kamer_id = hotelkamer.kamer_id WHERE boeking.member_id="+memberid)
     secondresult = mycursor.fetchall()
+
+
     jsonmemberinfo = json.dumps(secondresult, indent=4, sort_keys=True, default=str)
     return jsonmemberinfo
